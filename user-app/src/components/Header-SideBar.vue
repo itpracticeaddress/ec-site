f584e90b0b359e5c69e8cb40f6d3966110e41919
ShoYamawaki <it.practice.address@gmail.com>
2024-05-15 22:20:08 +0900

    Vueファイルを作成

--- user-app/src/components/Header-SideBar.vue
+++ user-app/src/components/Header-SideBar.vue
@@ -49,6 +49,10 @@ export default {
     {
       name: '購入履歴',
       url: '/shopping-history'
+    },
+    {
+      name: 'ログイン/ログアウト',
+      url: '/login'
     }
   ],
       headerItems: [
---
+++ user-app/src/views/ShoppingHistoryView.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>購入履歴画面</h1>
+  </template>
+  
+<script lang="ts">
+  
+  // Components
+  //import HelloWorld from '../components/HelloWorld.vue';
+  
+  export default ({
+    name: 'ShoppingHistoryView',
+  
+    components: {
+      
+    },
+  });
+  </script>
+  
